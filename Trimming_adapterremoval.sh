131e9e259ac18b0bcad8d045568a75ae3a94678f
jenwalshemond <jennifer.walsh.emond@gmail.com>
2021-01-20 13:19:29 -0500

    Updated - WGS club

--- Trimming_adapterremoval.sh
+++ Trimming_adapterremoval.sh
@@ -1,3 +1,11 @@
-##Trimming and Adapter Removal: All Samples
-##Bullock's Oriole (BUOR)
-/programs/adapterremoval_2.1.1/bin/AdapterRemoval --file1 /workdir/jlw395/oriole2_R1.fastq.gz --file2 /workdir/jlw395/oriole2_R2.fastq.gz --trimns --trimqualities --minquality 10 --collapse --threads 16 --adapter-list for_adapter_removal_2.txt --basename LIBRARY_BUOR1
+#Script for trimming and adpater removal
+#Modified on January 20, 2021
+
+#Trimns: Trim consecutive Ns from the 5’ and 3’ termini. If quality trimming is also enabled (--trimqualities), then stretches of mixed low-quality bases and/or Ns are trimmed.
+#Trimqualities: Trim consecutive stretches of low quality bases (threshold set by --minquality) from the 5’ and 3’ termini. If trimming of Ns is also enabled (--trimns), then stretches of mixed low-quality bases and Ns are trimmed.
+#minquality: Set the threshold for trimming low quality bases using --trimqualities and --trimwindows. Default is 2
+#collapse: In paired-end mode, merge overlapping mates into a single and recalculate the quality scores. In single-end mode, attempt to identify templates for which the entire sequence is available. In both cases, complete “collapsed” reads are written with a ‘M_’ name prefix, and “collapsed” reads which are trimmed due to quality settings are written with a ‘MT_’ name prefix.
+#adapterlist: Read one or more adapter sequences from a table. The first two columns (separated by whitespace) of each line in the file are expected to correspond to values passed to –adapter1 and –adapter2.
+#minlength: Reads shorter than this length are discarded following trimming. Defaults to 15Reads shorter than this length are discarded following trimming. Defaults to 15.
+
+/programs/adapterremoval_2.1.1/bin/AdapterRemoval --file1 Pathto/R1.fastq.gz --file2 Pathto/R2.fastq.gz --trimns --trimqualities --minquality 20 --collapse --threads 8 --adapter-list --minlength 25 for_adapter_removal_2.txt --basename LIBRARY_SAMPLEID
